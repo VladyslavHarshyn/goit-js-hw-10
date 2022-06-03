f889ec9d00b374d2c696441d1e753d31c1ea09fb
Vladyslav Harshyn <garshyn_98@ukr.net>
2022-06-03 11:59:19 +0300

    added api link to the proj

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import './css/styles.css';
-
+import { fetchCountries } from './fetchCountries';
 const DEBOUNCE_DELAY = 300;
+
+const searchBox = document.querySelector('#search-box');
+const countryList = document.querySelector('country-list');
+const countryInfo = document.querySelector('country-info');
+
+console.log(fetchCountries('Lebanon'));
